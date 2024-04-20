6359edb5c36f1c5f050337741b306e2bd4646d39
asjad-samdani <samdaniasjad143f@gmail.com>
2024-04-21 03:50:00 +0530

    react-redux toolkit

---
+++ reduxToolkit/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
+function App() {
+
+  return (  
+    <>
+    <h1>Redux toolkit</h1>
+    <AddTodo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
